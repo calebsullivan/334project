a7fe4289ddc8490a4b699f85dcfc75657856bef0
Caleb Sullivan <calebsullivan@me.com>
2014-06-03 18:01:20 -0400

    framework

---
+++ framework/head.php
@@ -0,0 +1,8 @@
+<head>
+	<!-- <meta -->
+	<title><?php //smack('title')?></title>
+	<link rel="stylesheet" type="text/css" href="/assets/css/html5reset.css"/>
+	<link rel="stylesheet" type="text/css" href="/assets/css/font-awesome.css"/>
+	<!-- <link rel="stylesheet" type="text/css" href="/assets/css/topcoat/css/ -->
+
+</head>
---
+++ framework/start.php
@@ -0,0 +1,8 @@
+<?php session_start();
+
+
+?><!DOCTYPE HTML>
+<html lang='en'>
+<?php require_once('head.php'); ?>
+<body>
+<div id="container">
